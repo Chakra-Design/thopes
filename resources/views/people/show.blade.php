fb216bb8d6cd977b5078e5b754509619ee9b8ca8
Rebecca Guradigudda <rebecca@chakradesign.co>
2020-01-30 15:27:29 +0530

    todays

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -1,13 +1,11 @@
 @extends('layout')
 @section('content')
-{{-- <ul>
-    <li><a href="/people/{{$Person->id}}"></a>
-</ul> --}}
+<ul>
+    <li><a href="/people/{{$new_person->id}}"></a>
+        <a href="/people/{{$new_person->name}}"></a>
+    </li>    
+</ul>
 
-<h2>{{$new_person->name}}</h2>
-<h2>{{$new_person->designation}}</h2>
-<h2>{{$new_person->profile_link}}</h2>
-<h2>{{$new_person->image_path}}</h2>
 
 
 @endsection
